2de5c8a0ff4377a2d1bd9b2c0a985803ae8e7cc6
procoderarham <The.Gamer.Thats.Him@gmail.com>
2025-06-11 00:18:40 -0400

    Add files via upload

---
+++ CARS.js
@@ -0,0 +1,23 @@
+function showfirstcar(){
+    
+        pagani.classList.remove("hide")
+        bugatti.classList.remove("hide")
+        lambo.classList.remove("hide")
+        lotus.classList.remove("hide")
+        rolls.classList.remove("hide")
+        Gclass.classList.remove("hide")
+        MGclass.classList.remove("hide")
+        ssc.classList.remove("hide")
+        avatar.classList.remove("hide")
+        purano.classList.remove("hide")
+        urus.classList.remove("hide")
+        ferr.classList.remove("hide")
+        porch.classList.remove("hide")
+        beamer.classList.remove("hide")
+        mclare.classList.remove("hide")
+        mclar.classList.remove("hide")
+        cone.classList.remove("hide")
+        niss.classList.remove("hide")
+        toy.classList.remove("hide")
+}
+btn1.onclick=showfirstcar;
